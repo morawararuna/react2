71c22879b4dde0365c5b7cba425b935d2eea9845
morawararuna <146064070+morawararuna@users.noreply.github.com>
2023-10-28 11:50:40 +0530

    Add files via upload

---
+++ withOutArugment.js
@@ -0,0 +1,13 @@
+function desingn(){
+    console.log("##################")
+
+}
+desingn()
+console.log("hello every one")
+desingn()
+console.log("good morning to all")
+desingn()
+console.log("it's java script programming")
+desingn()
+console.log("functions in java script")
+desingn()
